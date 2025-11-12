2319653a951fcd103b1615d9771579cee5c193da
Sakariya abuukar <146539781+Astrozaki122@users.noreply.github.com>
2025-11-12 09:33:08 +0000

    Add files via upload

---
+++ First-Attempt.php
@@ -0,0 +1,20 @@
+function mytest(){
+    $x = 5;
+   if ($x > 4){
+    echo "x is bigger than 4;
+   } else {
+    echo "x is not bigger than 4";
+   } 
+    $y = 8;
+
+    $z = $y * $x;
+
+    if ($z < 40  ){
+         echo "The value of z is: $z";
+    } else {
+        echo "z doesn't have a value.";
+    }
+   
+
+
+}
